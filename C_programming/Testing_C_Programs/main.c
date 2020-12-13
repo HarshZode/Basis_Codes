5cae4a36059c870a21af27efa072562483803ef6
Harsh Zode <zodeharsh@gmail.com>
2020-12-13 23:47:37 +0530

    One test code added to the teating file of C program.

--- C_programming/Testing_C_Programs/main.c
+++ C_programming/Testing_C_Programs/main.c
@@ -3,6 +3,15 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int a,b;
+    printf("This is My Addition Code which is running.\n");
+    printf("Enter the value of a\n");
+    scanf("%d",&a);
+    printf("Enter the value of b\n");
+    scanf("%d",&b);
+
+    printf("The summation of the values is %d", a+b);
+
+
     return 0;
 }
