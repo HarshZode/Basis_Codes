d64dfdca0c83cfa86f74d286d5ef5d59267247c6
Harsh Zode <zodeharsh@gmail.com>
2021-01-05 21:10:20 +0530

    New set of program made in testing file.

--- C_programming/Testing_C_Programs/main.c
+++ C_programming/Testing_C_Programs/main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main()
 {
-    int a,b;
-    printf("This is My Addition Code which is running.\n");
-    printf("Enter the value of a\n");
-    scanf("%d",&a);
-    printf("Enter the value of b\n");
-    scanf("%d",&b);
+    int physics, chemistry, math ;
+    float total;
+    printf("Enter marks of Physics\n");
+    scanf("%d",&physics);
+    printf("Enter marks of chemistry\n");
+    scanf("%d", &chemistry);
+    printf("Enter marks of maths\n");
+    scanf("%d", &math);
 
-    printf("The summation of the values is %d", a+b);
+    total = (physics + chemistry+ math)/3;
 
+   if (total <40 || physics <33 || chemistry <33 || math<33){
+    printf("\nYour Total is %f You failed.\n",total);
+   }
+   else {printf("\nYour Total is %f  You passed\n",total);}
 
     return 0;
 }
