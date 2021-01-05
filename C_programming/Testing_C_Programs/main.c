c03ff85f12ed4b24f9be3e1533c2eb3d9be16092
Harsh Zode <zodeharsh@gmail.com>
2021-01-05 21:48:43 +0530

    Improved the testing project

--- C_programming/Testing_C_Programs/main.c
+++ C_programming/Testing_C_Programs/main.c
@@ -12,13 +12,13 @@ int main()
     scanf("%f", &income);
 
     if (income >= 250000 && income <= 500000){
-        tax+= 0.05*(income - 250000);
+        tax=  tax + 0.05*( income - 250000);
     }
-    if (income>= 500000 && income <= 1000000){
-        tax+= 0.20*(income- 500000);
+    else if (income>= 500000 && income <= 1000000){
+        tax= tax + 0.20*(income- 500000);
     }
-    if (income >=100000){
-        tax+= 0.30*(income-1000000);
+    else if (income >=100000){
+        tax= tax + 0.30*(income-1000000);
     }
 
     printf("\nYour total tax to be paid is %f", tax);
