994fe549df82340bd1d5e27b945c35859f0241a9
Harsh Zode <zodeharsh@gmail.com>
2021-01-11 22:15:42 +0530

    Added some topics in previous notes.

--- C_programming/Learning_C/main.c
+++ C_programming/Learning_C/main.c
@@ -99,6 +99,143 @@ int main()
 
 */
 
+/*                           Conditional Instrutors
+
+
+            These are some functions which can decide which part of code is to be exicuted.
+
+            Code Example :-
+                            int a = 23;
+                            if (a<18){
+                                printf("You can drive.\n);
+                            }
+                            else{
+                                printf("You cannot drive");
+                            }
+
+
+
+
+                            LOGICAL OPERATORS
+
+
+            &&, ||, ! are three logic operators which are And, Or, Not respectively.
+
+            && -> And operator is true when both the conditions are true.
+            || -> Or operator is true when one of the conditions are true.
+            ! -> Not returns true if given false and false when given true.
+                    !(3==3) -> evaluated to be false.
+                    !(3== 21) -> evaluated as true.
+
+
+
+
+                            ELSE IF CLAUSE
+
+
+            Instead  of using multiole if statement, we can also use else if along with if
+            thus forming an if-else if-- else ladder.
+
+                    if {
+                        // Statement
+                    }
+                    else if {
+                        // Statement
+                    }
+                    else if {
+                        // Statement
+                    }
+                    else {
+                        // Statement
+                    }
+
+
+                            Switch Case Control Instruction
+
+
+                Switch case is used when we have to make a choice between number of
+                alternatives for a given variable.
+
+
+                    Swith(integre-expression)
+                    {
+                    Case C1:
+                            code;
+                    case c2 :
+                            code;  // C!, C2, C3 -> are constants
+                                   // code -> Any vaild C code.
+                    case c3 :
+                            code;
+                    default:
+                            code;
+                    }
+
+ The value of integer - Expression is matched against C1, C2, C3,....
+ If it matches any of these cases along with all subsequent "case" and
+ "default" statements are executed.
+
+ Important notes:
+                 1) We can use switch- case statements even by
+                   writing cases in any order of our choice (not necessarily ascending)
+                 2) Char values are allowed as they can be easily evaluated to an integer
+                 3) A switch can occur within another but in practice this is rarely used.
+
+
+*/
+
+/*                  Loops
+
+There is three types of loops;
+
+1) while loop :-
+
+    its syntex is
+
+    while('condition'){
+        function to be exicuted;
+        }
+
+2) do-while loop :-
+                    This is a special type of loop which
+                     first exicutes the thing inside and then
+                      checks the condition to repeat the loop again.
+
+    It's syntex is :-
+
+    do{
+    //Code//;
+    }while(condition)
+
+
+3) for loop :-
+                for (initialize ; condition; increment or decrement){
+                    //code;
+                    //code;
+                    //code;
+                }
+
+                initilize --> Setting a loop counter to an initial value
+                increment --> updating the loop counter
+
+        Eg:- for (i =0; i<3 ; i++){
+        printf("%d",i);
+        printf("\n");
+        }
+
+        The output will be:
+                            0
+                            1
+                            2
+
+ ++ --> increases the value by 1
+ -- --> Decreases the value by 1
+ *= --> multiplies the value by given number
+        for example :- a *= 2
+            this will multiply the initial value of a by 2.
+ /= --> This will divide the initial value by the value entered after equals sign (=).
+ */
+
+
 
 
 
